4318fe91540cc0c036522a01486d33c9e7a9ccfa
Matth <matthieu.defrenne@gmail.Com>
2018-05-26 10:47:43 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
+import { AddProductComponent } from './add-product/add-product.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product/:id', component: ProductComponent },
+  { path: 'add-product', component: AddProductComponent },
+  
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    HomeComponent,
+    AddProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
